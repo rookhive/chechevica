0561f5882612120df52b7c94b1cf2a3e14e0750c
Kirill Samoilov <rookhive@gmail.com>
2026-04-04 13:31:15 +0300

    Fix event propagation for external link clicks

--- src/shared/ui/ExternalLink.tsx
+++ src/shared/ui/ExternalLink.tsx
@@ -1,37 +1,39 @@
+import { openUrl } from '@tauri-apps/plugin-opener';
 import clsx from 'clsx';
 import { useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { Icon } from './Icon';
 import { Tooltip } from './Tooltip';
 
-export const ExternalLink = ({
-  href,
-  title,
-  iconSize = 20,
-  className,
-}: {
+type Props = {
   href: string;
   title?: string;
   iconSize?: number;
   className?: string;
-}) => {
-  const linkRef = useRef<HTMLAnchorElement>(null);
+};
+
+export const ExternalLink = ({ href, title, iconSize = 20, className }: Props) => {
+  const linkRef = useRef<HTMLSpanElement>(null);
+
   return (
     <>
-      <a
+      <span
         ref={linkRef}
         className={twMerge(
           clsx(
-            'inline-flex items-center rounded-full p-1.25 text-blue-300 transition-colors duration-300 hover:bg-white/15',
+            'inline-flex cursor-pointer items-center rounded-full p-1.25 text-blue-300 transition-colors duration-300 hover:bg-white/15',
             className
           )
         )}
-        href={href}
-        target="_blank"
-        rel="noreferrer noopener"
+        onClick={async (e) => {
+          linkRef.current?.blur();
+          e.preventDefault();
+          e.stopPropagation();
+          await openUrl(href);
+        }}
       >
         <Icon id="external-link" size={iconSize} />
-      </a>
+      </span>
       <Tooltip anchorRef={linkRef} position="top" status="info">
         {title}
       </Tooltip>
